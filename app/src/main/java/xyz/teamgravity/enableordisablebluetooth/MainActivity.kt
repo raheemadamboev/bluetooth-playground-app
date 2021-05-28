5270a39ca1746388ed2f39418db64cfbb760ca64
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-05-28 16:47:19 +0500

    discoverability enable disable

--- app/src/main/java/xyz/teamgravity/enableordisablebluetooth/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/enableordisablebluetooth/MainActivity.kt
@@ -20,40 +20,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        bluetoothAdapter()
+    }
+
+    private fun bluetoothAdapter() {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
-        button()
-    }
-
-    private fun button() {
-        onTurnOnOff()
-    }
-
-    // turn on off button
-    private fun onTurnOnOff() {
         binding.apply {
-
             // device has no bt capabilities
             if (bluetoothAdapter == null) {
                 onOffB.text = getString(R.string.not_available_bluetooth)
                 statusT.text = getString(R.string.not_available_bluetooth)
             } else {
-
-                // update ui
                 updateUI()
-
-                // register receiver
-                val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
-                registerReceiver(receiver, filter)
-
-                // button
-                onOffB.setOnClickListener {
-                    if (bluetoothAdapter!!.isEnabled) {
-                        bluetoothAdapter!!.disable()
-                    } else {
-                        startActivity(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
-                    }
-                }
+                button()
             }
         }
     }
@@ -64,6 +44,13 @@ class MainActivity : AppCompatActivity() {
         } else {
             updateBluetoothOff()
         }
+
+        updateDiscoverability()
+    }
+
+    private fun button() {
+        onTurnOnOff()
+        onDiscoverability()
     }
 
     private fun updateBluetoothOn() {
@@ -80,26 +67,93 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun updateDiscoverability() {
+        binding.discoverabilityT.text =
+            getString(if (bluetoothAdapter!!.isDiscovering) R.string.discoverability_enabled else R.string.discoverability_disabled)
+    }
+
+    // turn on off button
+    private fun onTurnOnOff() {
+        binding.apply {
+
+            // register receiver
+            val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+            registerReceiver(receiver, filter)
+
+            // button
+            onOffB.setOnClickListener {
+                if (bluetoothAdapter!!.isEnabled) {
+                    bluetoothAdapter!!.disable()
+                } else {
+                    startActivity(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
+                }
+            }
+        }
+    }
+
+    // on off discoverability
+    private fun onDiscoverability() {
+        val intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+        intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 10)
+
+        val filter = IntentFilter(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)
+        registerReceiver(receiver, filter)
+
+        binding.discoverabilityB.setOnClickListener {
+            startActivity(intent)
+        }
+    }
+
     // respond to bluetooth changes
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            if (intent?.action == BluetoothAdapter.ACTION_STATE_CHANGED) {
-                binding.apply {
-                    when (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)) {
-                        BluetoothAdapter.STATE_OFF -> {
-                            updateBluetoothOff()
-                        }
+            binding.apply {
+                when (intent?.action) {
 
-                        BluetoothAdapter.STATE_TURNING_OFF -> {
-                            statusT.text = getString(R.string.turning_off)
-                        }
+                    // turn off on state
+                    BluetoothAdapter.ACTION_STATE_CHANGED -> {
+                        when (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)) {
+                            BluetoothAdapter.STATE_OFF -> {
+                                updateBluetoothOff()
+                            }
 
-                        BluetoothAdapter.STATE_ON -> {
-                            updateBluetoothOn()
-                        }
+                            BluetoothAdapter.STATE_TURNING_OFF -> {
+                                statusT.text = getString(R.string.turning_off)
+                            }
 
-                        BluetoothAdapter.STATE_TURNING_ON -> {
-                            statusT.text = getString(R.string.turning_on)
+                            BluetoothAdapter.STATE_ON -> {
+                                updateBluetoothOn()
+                            }
+
+                            BluetoothAdapter.STATE_TURNING_ON -> {
+                                statusT.text = getString(R.string.turning_on)
+                            }
+                        }
+                    }
+
+                    // discoverability
+                    BluetoothAdapter.ACTION_SCAN_MODE_CHANGED -> {
+                        when (intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR)) {
+
+                            BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE -> {
+                                discoverabilityT.text = getString(R.string.discoverability_enabled)
+                            }
+
+                            BluetoothAdapter.SCAN_MODE_CONNECTABLE -> {
+                                discoverabilityT.text = getString(R.string.connectable)
+                            }
+
+                            BluetoothAdapter.SCAN_MODE_NONE -> {
+                                discoverabilityT.text = getString(R.string.discoverability_disabled_none)
+                            }
+
+                            BluetoothAdapter.STATE_CONNECTING -> {
+                                discoverabilityT.text = getString(R.string.connecting)
+                            }
+
+                            BluetoothAdapter.STATE_CONNECTED -> {
+                                discoverabilityT.text = getString(R.string.connected)
+                            }
                         }
                     }
                 }
